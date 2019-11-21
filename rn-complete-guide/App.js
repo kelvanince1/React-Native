d2484e210464416e60bda5b72164689ca68591c3
Kelvan Ince <incekelvan@gmail.com>
2019-11-21 10:20:18 +0700

    Add flexbox to project

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -4,9 +4,9 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 export default function App() {
   return (
     <View style={{ padding: 50 }}>
-      <View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
         <TextInput
-          style={{ borderColor: 'black', borderWidth: 1, padding: 10 }}
+          style={{ borderColor: 'black', borderWidth: 1, padding: 10, width: 200 }}
           placeholder="Course Goal"
         />
         <Button
