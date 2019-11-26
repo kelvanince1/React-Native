f91bd30b38ff63a5f9d3ae2f55f4087cd7be4082
Kelvan Ince <incekelvan@gmail.com>
2019-11-26 07:39:17 -0500

    Style image on Game Over screen

--- number-game/screens/GameOver/index.js
+++ number-game/screens/GameOver/index.js
@@ -8,7 +8,9 @@ const GameOver = props => {
     return (
         <View style={styles.screen}>
             <TitleText>The game is over</TitleText>
-            <Image source={require('../../assets/success.png')} />
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={require('../../assets/success.png')} />
+            </View>
             <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
             <BodyText>Number was: {props.userNumber}</BodyText>
             <Button
@@ -24,6 +26,19 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image: {
+        width: '100%',
+        height: '100%'
     }
 });
 
