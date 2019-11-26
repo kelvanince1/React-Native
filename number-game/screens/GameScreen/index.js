93e3a0dece72507b871bfd78daba72ffcbf918bb
Kelvan Ince <incekelvan@gmail.com>
2019-11-26 01:30:25 -0500

    Add useEffect hook to render the game over once the correct number is identified

--- number-game/screens/GameScreen/index.js
+++ number-game/screens/GameScreen/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 import NumberContainer from '../../components/NumberContainer';
@@ -19,12 +19,21 @@ const generateRandomBetween = (min, max, exclude) => {
 
 const GameScreen = props => {
     const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1,100,props.userChoice));
+    const [rounds, setRounds] = useState(0);
 
     const currentLow = useRef(1);
     const currentHigh = useRef(100);
 
+    const { userChoice, onGameOver } = props;
+
+    useEffect(() => {
+        if (currentGuess === userChoice) {
+            onGameOver(rounds);
+        }
+    }, [currentGuess, userChoice, onGameOver]);
+
     const nextGuessHandler = direction => {
-        if ((direction === 'lower' && currentGuess < props.userChoice) || (direction === 'greater' && currentGuess > props.userChoice)) {
+        if ((direction === 'lower' && currentGuess < userChoice) || (direction === 'greater' && currentGuess > props.userChoice)) {
             Alert.alert('Incorrect', 'You know this is wrong!', [{text: 'Sorry', style: 'cancel'}]);
             return;
         }
@@ -37,6 +46,7 @@ const GameScreen = props => {
 
         const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
         setCurrentGuess(nextNumber);
+        setRounds(curRounds => curRounds + 1);
     };
 
     return (
