6ae36a8bbaa406177490906299a3f795d9840585
Kelvan Ince <incekelvan@gmail.com>
2019-11-26 05:45:56 -0500

    Add logic to restart game once correct number is found

--- number-game/screens/GameOver/index.js
+++ number-game/screens/GameOver/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 const GameOver = props => {
     return (
         <View style={styles.screen}>
             <Text>The game is over</Text>
+            <Text>Number of rounds: {props.roundsNumber}</Text>
+            <Text>Number was: {props.userNumber}</Text>
+            <Button
+                title="New Game"
+                onPress={props.onRestart}
+            />
         </View>
     );
 }
