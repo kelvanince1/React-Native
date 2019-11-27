9ad027bb8858fd5248bd456175a4e9090c2ae5f6
Kelvan Ince <incekelvan@gmail.com>
2019-11-27 07:41:56 -0500

    More responsive styling

--- number-game/screens/GameScreen/index.js
+++ number-game/screens/GameScreen/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, Alert, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, Alert, ScrollView, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import NumberContainer from '../../components/NumberContainer';
@@ -104,13 +104,13 @@ const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
-        marginTop: 20,
+        marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
         width: 400,
         maxWidth: '90%'
     },
     listContainer: {
         flex: 1,
-        width: '80%'
+        width: Dimensions.get('window') > 350 ? '60%' : '80%'
     },
     list: {
         flexGrow: 1,
@@ -125,7 +125,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         flexDirection: 'row',
         justifyContent: 'space-around',
-        width: '60%'
+        width: '100%'
     },
 });
 
